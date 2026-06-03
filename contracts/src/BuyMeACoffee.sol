d34d39e7ecf20e48aba3c2c686aad740cfa8fcf5
dor-rondel <dor.rondel@gmail.com>
2026-06-03 21:36:08 +0300

    finished v0.1 final touches, docs, etc, added gh-pages action

--- contracts/src/BuyMeACoffee.sol
+++ contracts/src/BuyMeACoffee.sol
@@ -29,11 +29,16 @@ contract BuyMeACoffee {
         _;
     }
 
+    /// @notice Initializes the contract with a price feed address
+    /// @param priceFeed The address of the Chainlink price feed
     constructor(address priceFeed) {
         i_priceFeed = AggregatorV3Interface(priceFeed);
         i_owner = msg.sender;
     }
 
+    /// @notice Allows a user to donate ETH to the contract
+    /// @dev Requires a minimum contribution in USD
+    /// @param message A message to include with the donation (max 200 chars)
     function fund(string memory message) public payable {
         if (msg.value.getConversionRate(i_priceFeed) < MINIMUM_USD) {
             revert BuyMeACoffee__NotEnoughETH();
@@ -45,23 +50,33 @@ contract BuyMeACoffee {
         s_donors.push(Donor({donor: msg.sender, message: message, timestamp: block.timestamp, amount: msg.value}));
     }
 
+    /// @notice Allows the owner to withdraw all funds
     function withdraw() public onlyOwner {
         (bool success,) = i_owner.call{value: address(this).balance}("");
         if (!success) revert BuyMeACoffee__WithdrawFailed();
     }
 
+    /// @notice Returns a specific donor by index
+    /// @param index The index of the donor to retrieve
+    /// @return A Donor struct
     function getDonor(uint256 index) public view returns (Donor memory) {
         return s_donors[index];
     }
 
+    /// @notice Returns all donors
+    /// @return An array of all Donor structs
     function getDonors() public view returns (Donor[] memory) {
         return s_donors;
     }
 
+    /// @notice Returns the contract owner
+    /// @return The owner's address
     function getOwner() public view returns (address) {
         return i_owner;
     }
 
+    /// @notice Returns the total number of donors
+    /// @return The number of donors
     function getNumberOfDonors() public view returns (uint256) {
         return s_donors.length;
     }
